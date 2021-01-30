b748541b4351c13250b8d4ab7825b809950b245b
SAJAL CHAKRABORTY <38472281+sajalch@users.noreply.github.com>
2021-01-30 16:16:17 +0530

    trial 1

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,11 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
+	rec2.top=Math.abs(Number(rec2.top.substr(0,rec2.top.length-2))-Number(rec1.top.substr(0,rec1.top.length-2)))+"px";
+	rec2.left=Math.abs(Number(rec2.left.substr(0,rec2.left.length-2))-Number(rec1.left.substr(0,rec1.left.length-2)))+"px";
+	rec2.height=Math.abs(Number(rec2.height.substr(0,rec2.height.length-2))-Number(rec1.height.substr(0,rec1.height.length-2)))+"px";
+	rec2.width=Math.abs(Number(rec2.width.substr(0,rec2.width.length-2))-Number(rec1.width.substr(0,rec1.width.length-2)))+"px";
+	return rec1.children.push(rec2);
 }
 
 module.exports = updateStructure;
