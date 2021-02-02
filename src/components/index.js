5d961943723095e90d1db2d0ae2da1aa195c7d19
SAJAL CHAKRABORTY <38472281+sajalch@users.noreply.github.com>
2021-02-02 20:00:30 +0530

    trail 4

--- src/components/index.js
+++ src/components/index.js
@@ -6,40 +6,99 @@
 //      children: []
 //}
 
-function isInside(top,left,height,width){
-    if(top>=0 && left>=0 && height+top <=0 && width+left <=0)
-        return 0;
-    else if(top<0 && left<0 && height+top > 0 && width+left > 0)
-        return 1;
-    return -1;
-}
-function updateStructure(rec1,rec2){
-	//write your code
-	let top=Number(rec2.top.substr(0,rec2.top.length-2))-Number(rec1.top.substr(0,rec1.top.length-2));
-	let left=Number(rec2.left.substr(0,rec2.left.length-2))-Number(rec1.left.substr(0,rec1.left.length-2));
-	let height=Number(rec2.height.substr(0,rec2.height.length-2))-Number(rec1.height.substr(0,rec1.height.length-2));
-	let width=Number(rec2.width.substr(0,rec2.width.length-2))-Number(rec1.width.substr(0,rec1.width.length-2));
+// function isInside(top,left,height,width){
+//     if(top>=0 && left>=0 && height+top <=0 && width+left <=0)
+//         return 0;
+//     else if(top<0 && left<0 && height+top > 0 && width+left > 0)
+//         return 1;
+//     return -1;
+// }
+// function updateStructure(rec1,rec2){
+// 	//write your code
+// 	let top=Number(rec2.top.substr(0,rec2.top.length-2))-Number(rec1.top.substr(0,rec1.top.length-2));
+// 	let left=Number(rec2.left.substr(0,rec2.left.length-2))-Number(rec1.left.substr(0,rec1.left.length-2));
+// 	let height=Number(rec2.height.substr(0,rec2.height.length-2))-Number(rec1.height.substr(0,rec1.height.length-2));
+// 	let width=Number(rec2.width.substr(0,rec2.width.length-2))-Number(rec1.width.substr(0,rec1.width.length-2));
 	
-	if(isInside(top,left,height,width)===0){
+// 	if(isInside(top,left,height,width)===0){
 		
-		rec={...rec1,
-			children:[{top:Math.abs(top)+"px",
-			left:Math.abs(left)+"px",
-			height:Math.abs(height)+"px",
-			width:Math.abs(width)+"px"}]
-		}
-	 	return rec;
-	}else if(isInside(top,left,height,width)===1){
-		rec={...rec2,
-			children:[{top:Math.abs(top)+"px",
-			left:Math.abs(left)+"px",
-			height:Math.abs(height)+"px",
-			width:Math.abs(width)+"px"}]
-		}
-	 	return rec;
-	}
-	rec={...rec1};
-	 return rec;
-}
-
+// 		rec={...rec1,
+// 			children:[{top:Math.abs(top)+"px",
+// 			left:Math.abs(left)+"px",
+// 			height:Math.abs(height)+"px",
+// 			width:Math.abs(width)+"px"}]
+// 		}
+// 	 	return rec;
+// 	}else if(isInside(top,left,height,width)===1){
+// 		rec={...rec2,
+// 			children:[{top:Math.abs(top)+"px",
+// 			left:Math.abs(left)+"px",
+// 			height:Math.abs(height)+"px",
+// 			width:Math.abs(width)+"px"}]
+// 		}
+// 	 	return rec;
+// 	}
+// 	rec={...rec1};
+// 	 return rec;
+// }
+function updateStructure(rec1,rec2){
+        //write your code
+        var top1 = rec1.top, top2 = rec2.top;
+        var left1 = rec1.left, left2 = rec2.left;
+        var width1 = rec1.width, width2 = rec2.width;
+        var height1 = rec1.height, height2 = rec2.height;
+            top1 = top1.slice(0,-1); top1 = top1.slice(0,-1);
+            top1 = Number(top1);
+            top2 = top2.slice(0,-1); top2 = top2.slice(0,-1);
+            top2 = Number(top2);
+            left1 = left1.slice(0,-1); left1 = left1.slice(0,-1);
+            left1 = Number(left1);
+            left2 = left2.slice(0,-1); left2 = left2.slice(0,-1);
+            left2 = Number(left2);
+            width1 = width1.slice(0,-1); width1 = width1.slice(0,-1);
+            width1 = Number(width1);
+            width2 = width2.slice(0,-1); width2 = width2.slice(0,-1);
+            width2 = Number(width2);
+            height1 = height1.slice(0,-1); height1 = height1.slice(0,-1);
+            height1 = Number(height1);
+            height2 = height2.slice(0,-1); height2 = height2.slice(0,-1);
+            height2 = Number(height2);
+            if(top1 > top2 && left1 > left2 && (top2+height2) > (top1+height1) && (left2 + width2) >(left1 + width1)){
+                return {
+                    top: rec2.top,
+                    left: rec2.left,
+                    width: rec2.width,
+                    height: rec2.height,
+                        children: [{
+                        top: `${top1-top2}px`,
+                        left: `${left1-left2}px`,
+                        width: rec1.width,
+                        height: rec1.height,
+                        children: []
+                    }]
+                }
+            }else if(top1 <= top2 && left1 <= left2 && (top2+height2) <= (top1+height1) && (left2 + width2) <= (left1 + width1)){
+                return {
+                    top: rec1.top,
+                    left: rec1.left,
+                    width: rec1.width,
+                    height: rec1.height,
+                        children: [{
+                        top: `${top2-top1}px`,
+                        left: `${left2-left1}px`,
+                        width: rec2.width,
+                        height: rec2.height,
+                        children: []
+                    }]
+                }
+            }else {
+                return {
+                    top: rec1.top,
+                    left: rec1.left,
+                    width: rec1.width,
+                    height: rec1.height,
+                    children: []
+                }
+            }
+        }
 module.exports = updateStructure;
