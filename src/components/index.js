0f4e92ebbe507517e166c3b6bec478d43e4c6aba
SAJAL CHAKRABORTY <38472281+sajalch@users.noreply.github.com>
2021-02-03 17:33:30 +0530

    trail 9

--- src/components/index.js
+++ src/components/index.js
@@ -47,21 +47,21 @@ function updateStructure(rec1,rec2){
         var left1 = rec1.left, left2 = rec2.left;
         var width1 = rec1.width, width2 = rec2.width;
         var height1 = rec1.height, height2 = rec2.height;
-            top1 = top1.slice(0,-1); top1 = top1.slice(0,-1);
+            top1 = top1.slice(0,-2); top1 = top1.slice(0,-2);
             top1 = Number(top1);
-            top2 = top2.slice(0,-1); top2 = top2.slice(0,-1);
+            top2 = top2.slice(0,-2); top2 = top2.slice(0,-2);
             top2 = Number(top2);
-            left1 = left1.slice(0,-1); left1 = left1.slice(0,-1);
+            left1 = left1.slice(0,-2); left1 = left1.slice(0,-2);
             left1 = Number(left1);
-            left2 = left2.slice(0,-1); left2 = left2.slice(0,-1);
+            left2 = left2.slice(0,-2); left2 = left2.slice(0,-2);
             left2 = Number(left2);
-            width1 = width1.slice(0,-1); width1 = width1.slice(0,-1);
+            width1 = width1.slice(0,-2); width1 = width1.slice(0,-2);
             width1 = Number(width1);
-            width2 = width2.slice(0,-1); width2 = width2.slice(0,-1);
+            width2 = width2.slice(0,-2); width2 = width2.slice(0,-2);
             width2 = Number(width2);
-            height1 = height1.slice(0,-1); height1 = height1.slice(0,-1);
+            height1 = height1.slice(0,-2); height1 = height1.slice(0,-2);
             height1 = Number(height1);
-            height2 = height2.slice(0,-1); height2 = height2.slice(0,-1);
+            height2 = height2.slice(0,-2); height2 = height2.slice(0,-2);
             height2 = Number(height2);
             if(top1 > top2 && left1 > left2 && (top2+height2) > (top1+height1) && (left2+width2) >(left1+width1)){
                 return {
